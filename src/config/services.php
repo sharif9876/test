f0d7152e69e575377a2281ec299738643e3ec643
Guus Urselmann <guusurselmann@gmail.com>
2019-01-11 17:47:46 +0100

    small setup fixes fb auth

--- src/config/services.php
+++ src/config/services.php
@@ -45,5 +45,11 @@ return [
         'client_secret' => env('TWITTER_SECRET'),
         'redirect'      => env('TWITTER_URL'),
     ],
+    
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('CALLBACK_URL_FACEBOOK'),
+    ]
 
 ];
